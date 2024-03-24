ab49389c926905bd4f1f409c03aac2a4549dc9cb
Nikesh Bajaj <bajaj.nikkey@gmail.com>
2024-03-24 00:59:48 +0000

    Update versionwarning.js

--- versionwarning.js
+++ versionwarning.js
@@ -1,5 +1,5 @@
 (function() {
-    var latestStable = '1.4';
+    var latestStable = '0.0.9.6';
     var goodPaths = ['stable', 'dev', latestStable];
     var showWarning = (msg) => {
         $('.body[role=main]').prepend(
@@ -12,7 +12,7 @@
             ].join('; ') +
             '">' + msg + '</p>')
     };
-    if (location.hostname == 'scikit-learn.org') {
+    if (location.hostname == 'spkit.github.io') {
         if (location.pathname == '/dev/versions.html') {
             // this page is specially generated and linked from all versions
             return;
@@ -20,17 +20,17 @@
         var versionPath = location.pathname.split('/')[1];
         if (!goodPaths.includes(versionPath)) {
             showWarning('This is documentation for an old release of ' +
-                        'Scikit-learn (version ' + versionPath + '). Try the ' +
-                        '<a href="https://scikit-learn.org">latest stable ' +
+                        'SpKit (version ' + versionPath + '). Try the ' +
+                        '<a href="https://SpKit.github.io">latest stable ' +
                         'release</a> (version ' + latestStable + ') or ' +
-                        '<a href="https://scikit-learn.org/dev">development</a> ' +
+                        '<a href="https://SpKit.github.io/dev">development</a> ' +
                         '(unstable) versions.')
         } else if (versionPath == 'dev') {
             showWarning('This is documentation for the unstable ' +
-                        'development version of Scikit-learn. (To use it, ' +
-                        '<a href="https://scikit-learn.org/stable/developers/advanced_installation.html#installing-nightly-builds">install the nightly build</a>.) ' + 
+                        'development version of SpKit. (To use it, ' +
+                        '<a href="https://SpKit.github.io.org/stable/developers/advanced_installation.html#installing-nightly-builds">install the nightly build</a>.) ' + 
                         'The latest stable ' +
-                        'release is <a href="https://scikit-learn.org">version ' +
+                        'release is <a href="https://SpKit.github.io">version ' +
                         latestStable + '</a>.')
         }
     }
